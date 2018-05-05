7ea4b24485a5e21550a29b5859addd11a2eecf8b
anuragbandhu <anuragbandhu007@gmail.com>
2018-05-05 14:31:26 +0530

    Bug fixed

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREFOX_API_KEY,
-  authDomain: process.env.FIREFOX_AUTH_DOMAIN,
-  databaseURL: process.env.FIREFOX_DATABASE_URL,
-  projectId: process.env.FIREFOX_PROJECT_ID,
-  storageBucket: process.env.FIREFOX_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREFOX_MESSAGING_SENDER_ID
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
